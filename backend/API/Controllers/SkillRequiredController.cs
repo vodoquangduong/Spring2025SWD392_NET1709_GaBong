6bc2a39359e32f8f2a6833e31cb68cd5dd1ca867
Vo Do Quang Duong <quangduongaptx4869@gmail.com>
2025-02-26 22:36:56 +0700

    feat: add skillRequired Project API

---
+++ backend/API/Controllers/SkillRequiredController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillRequiredController : ControllerBase
+    {
+
+
+        private readonly ISkillRequiredService _skillRequiredService;
+
+        public SkillRequiredController(ISkillRequiredService skillRequiredService)
+        {
+            _skillRequiredService = skillRequiredService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProjectSkill([FromRoute]long id)
+        {
+            var skills = await _skillRequiredService.GetSkillByProjectIdAsync(id);
+            if (skills == null)
+            {
+                return NotFound("Skill required of this project not found");
+            }
+            return Ok(skills);
+        }
+    }
+}
---
+++ backend/Services/Interfaces/ISkillRequiredService.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObjects.Models;
+using Helpers.DTOs.SkillCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ISkillRequiredService
+    {
+        Task<List<SkillCategory>> GetSkillByProjectIdAsync(long id);
+
+    }
+}
